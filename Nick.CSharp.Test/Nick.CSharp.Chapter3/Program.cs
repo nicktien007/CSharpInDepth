4d4215d45d97156c23515c5476433c830ec41c2f
nicktien <trojanhorse0077@gmail.com>
2018-08-29 09:47:17 +0800

    3-2練習

---
+++ Nick.CSharp.Test/Nick.CSharp.Chapter3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nick.CSharp.Chapter3
+{
+    /// <summary>
+    /// 3-2 p60
+    /// </summary>
+    class Program
+    {
+        static double TakeSquareRoot(int x)
+        {
+            return Math.Sqrt(x);
+        }
+        static void Main(string[] args)
+        {
+            List<int> integers = new List<int> {1, 2, 3, 4};
+
+            Converter<int, double> converter = TakeSquareRoot;
+            List<double> doubles = integers.ConvertAll<double>(converter);
+
+            foreach (double d in doubles)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
--- Nick.CSharp.Test/Nick.CSharp.Test/Program.cs
+++ Nick.CSharp.Test/Nick.CSharp.Test/Program.cs
@@ -10,6 +10,18 @@ namespace Nick.CSharp.Test
     {
         static void Main(string[] args)
         {
+            //代碼2-5 p46
+            //Func<int, int, string> func = (x, y) => (x * y).ToString();
+            //Console.WriteLine(func(5,20));
+
+            var a = new {Name = "a", Age = 29};
+            var b = new {Name = "b", Age = 88};
+
+            var c = new { Name = "b", Age = 88,Height=178 };
+            
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(c.GetType());
         }
     }
 }
