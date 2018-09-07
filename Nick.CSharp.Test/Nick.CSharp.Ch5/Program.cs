c276025a3fe26303b3e9bafed8fdd4872539117b
nicktien <trojanhorse0077@gmail.com>
2018-09-07 19:06:28 +0800

    5-3

---
+++ Nick.CSharp.Test/Nick.CSharp.Ch5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nick.CSharp.Ch5
+{
+    class Program
+    {
+        delegate Stream StreamFactory();
+
+        static MemoryStream GererateSampleData()
+        {
+            byte[] buffer = new byte[16];
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = (byte) i;
+            }
+
+            return new MemoryStream(buffer);
+        }
+        static void Main(string[] args)
+        {
+            //5-3
+            StreamFactory factory = GererateSampleData;
+
+            using (Stream stream = factory())
+            {
+                int data;
+                while ((data = stream.ReadByte())!=-1)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
